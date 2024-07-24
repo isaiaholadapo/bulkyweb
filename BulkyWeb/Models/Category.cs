5c6be0c04552b9eabc8e2f7469109f01b1c03bb5
Isaiah Olatunbosun <daezyworld@gmail.com>
2024-07-24 20:20:59 +0100

    first commit

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+}
